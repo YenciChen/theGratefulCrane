c17cee3e085f34d3b8a5efff86e96665d6b7b5cd
Yenci <a278862002@gmail.com>
2023-07-25 18:46:40 +0800

    Initial Commit

---
+++ theGratefulCrane/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  theGratefulCrane
+//
+//  Created by 陳妍慈 on 2023/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
